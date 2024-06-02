0de70376c68f7744cb281c8d339c887ae753e62d
imfreeman1 <dkfrhkt@gmail.com>
2024-06-02 16:12:55 +0900

    .env 설정

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,19 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
   plugins: [
+    [
+      'module:react-native-dotenv',
+      {
+        envName: 'MY_ENV',
+        moduleName: '@env',
+        path: '.env.local',
+        blocklist: null,
+        allowlist: null,
+        safe: false,
+        allowUndefined: true,
+        verbose: false,
+      },
+    ],
     'react-native-reanimated/plugin',
     [
       'module-resolver',
