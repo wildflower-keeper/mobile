45da0c2d46944f4bc3ac5329f92fd7e434d25b71
imfreeman1 <dkfrhkt@gmail.com>
2024-06-03 18:48:55 +0900

    backendAxiosInstance의 baseURL 변경
     - 안드로이드에서는 localhost가 아닌 해당 기기의 ip주소를 통해 통신이 가능해서 변경하였습니다.

--- src/utils/api/api.ts
+++ src/utils/api/api.ts
@@ -1,7 +1,12 @@
 import axios from 'axios';
 
+const backendAxiosInstance = axios.create({
+  baseURL: 'http://10.0.2.2:8080',
+  withCredentials: true,
+});
+
 const weatherAxiosInstance = axios.create({
   baseURL: 'https://api.openweathermap.org',
 });
 
-export {weatherAxiosInstance};
+export {backendAxiosInstance, weatherAxiosInstance};
