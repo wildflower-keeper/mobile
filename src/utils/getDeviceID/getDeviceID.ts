e893922198ae6e576739d8a0ab3e842e91488abc
imfreeman1 <dkfrhkt@gmail.com>
2024-06-16 18:07:33 +0900

    인증 관련 통신 함수 정리

--- src/utils/getDeviceID/getDeviceID.ts
+++
@@ -1,8 +0,0 @@
-import DeviceInfo from 'react-native-device-info';
-
-const getDeviceID = async () => {
-  const deviceID = await DeviceInfo.getUniqueId();
-  return deviceID;
-};
-
-export default getDeviceID;
