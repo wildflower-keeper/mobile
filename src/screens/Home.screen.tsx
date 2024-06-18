18be31eca822c7cb5cf0bae5752df272e4323c77
imfreeman1 <dkfrhkt@gmail.com>
2024-06-18 17:03:27 +0900

    홈 레이아웃 변경

--- src/screens/Home.screen.tsx
+++ src/screens/Home.screen.tsx
@@ -48,61 +48,116 @@ const Home = ({navigation}: HomeProps) => {
 
   return (
     <SafeAreaView style={styles.container}>
-      <View style={styles.headContainer}>
-        <CustomText textColor="weak">
-          {isSuccess && `${userInfo.shelterName} -`}
-        </CustomText>
-        <CustomText size="xLarge">
-          {isSuccess && `${userInfo.homelessName}님, 반갑습니다.`}
-        </CustomText>
-      </View>
-      <View style={styles.weatherContainer}>
-        <CustomText>{`${today.getMonth() + 1}월 ${today.getDate()}일 ${
-          days.WEEK[today.getDay()]
-        }요일`}</CustomText>
-        <CustomText>위치</CustomText>
-        <View style={{flex: 0, flexDirection: 'row', gap: 10}}>
-          <CustomText>{currentWeather.weather}</CustomText>
-          <CustomText size="temp">{currentWeather.temp}&#176;</CustomText>
+      <View style={styles.outContainer}>
+        <View style={styles.headerContainer}>
+          <View style={styles.headContainer}>
+            <CustomText textColor="weak">
+              {isSuccess && `${userInfo.shelterName}`}
+            </CustomText>
+            <CustomText>{`${today.getMonth() + 1}월 ${today.getDate()}일 ${
+              days.WEEK[today.getDay()]
+            }요일`}</CustomText>
+            <CustomText>
+              {isSuccess && `${userInfo.homelessName}님, 반갑습니다.`}
+            </CustomText>
+          </View>
+          <View style={styles.weatherContainer}>
+            <View style={{flex: 0, flexDirection: 'row', gap: 10}}>
+              <CustomText>{currentWeather.weather}</CustomText>
+              <CustomText size="xLarge">{currentWeather.temp}&#176;</CustomText>
+            </View>
+          </View>
+        </View>
+        <View style={styles.messageContainer}>
+          <CustomText textColor="weak" size="small">
+            외출 시 약을 꼭 챙겨주세요!
+          </CustomText>
         </View>
       </View>
-      <View style={styles.medicationContainer}>
-        <CustomText textColor="weak" size="large">
-          복약권유
-        </CustomText>
-        <CustomText textColor="weak">복약정보, 1회필요량, 복약률 </CustomText>
+      <View style={styles.bodyContainer}>
+        <View style={styles.medicationContainer}>
+          <CustomText size="large">긴급 도움 요청 버튼 자리</CustomText>
+        </View>
+        <View style={styles.medicationContainer}>
+          <CustomText textColor="white" size="large">
+            복약권유
+          </CustomText>
+          <CustomText textColor="white">복약정보, 1회필요량, 복약률</CustomText>
+        </View>
+        <View style={styles.buttonContainer}>
+          <CustomButton
+            size="md"
+            label="외박 신청하기"
+            onPress={() => navigation.navigate('OvernightRequest')}
+          />
+          <CustomButton
+            size="md"
+            label="외박 신청내역"
+            variant="outlined"
+            onPress={() => navigation.navigate('OvernightRequest')}
+          />
+        </View>
       </View>
-      <CustomButton
-        size="xl"
-        label="외박 신청하기"
-        onPress={() => navigation.navigate('OvernightRequest')}
-      />
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    marginVertical: 72,
     flex: 1,
     alignItems: 'center',
     gap: 16,
-    marginHorizontal: 30,
+  },
+  outContainer: {
+    flex: 0,
+    paddingVertical: 20,
+    paddingHorizontal: 10,
+    backgroundColor: '#D9D9D9',
+    borderBottomLeftRadius: 30,
+    borderBottomRightRadius: 30,
+    width: '100%',
+    gap: 22,
+  },
+  messageContainer: {
+    backgroundColor: colors.WHITE,
+    paddingHorizontal: 24,
+    paddingVertical: 14,
+    borderRadius: 30,
+    borderColor: colors.FONT_WEAK,
+    borderWidth: 1,
+  },
+  headerContainer: {
+    paddingHorizontal: 10,
+    flex: 0,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  bodyContainer: {
+    flex: 1,
+    width: '95%',
+    marginHorizontal: 10,
+    gap: 10,
+    marginVertical: 16,
   },
   headContainer: {
     flex: 0,
-    alignItems: 'center',
+    gap: 6,
   },
   weatherContainer: {
-    flex: 1,
-    alignItems: 'center',
-  },
-  medicationContainer: {
     flex: 0,
     alignItems: 'center',
-    borderColor: colors.BORDER_COLOR,
-    borderWidth: 6,
-    borderRadius: 10,
+  },
+  buttonContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    gap: 10,
+    width: '100%',
+  },
+  medicationContainer: {
+    flex: 1,
+    alignItems: 'center',
+    backgroundColor: '#D9D9D9',
+    borderRadius: 20,
     padding: 16,
     width: '100%',
   },
