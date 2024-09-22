1b9a3ebe969725e316d930a22eca750e21efca41
imfreeman1 <dkfrhkt@gmail.com>
2024-09-22 20:12:58 +0900

    [fix] console.log 삭제

--- src/screens/AuthSignup.screen.tsx
+++ src/screens/AuthSignup.screen.tsx
@@ -113,7 +113,6 @@ const AuthSignup = ({}: AuthSignupProps) => {
 
   const handleSubmit = async () => {
     try {
-      console.log(signupValues);
       const res = await backendAxiosInstance.post(
         '/api/v1/homeless-app/homeless',
         signupValues,
