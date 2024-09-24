9871890a13b53e24244802aa4ed722ef55e00e31
imfreeman1 <dkfrhkt@gmail.com>
2024-09-24 14:46:11 +0900

    [hotfix] 모달 텍스트 로직 재수정
     - deepLinkData의 값에 따라 모달 string 설정되도록 변경

--- src/screens/ScanResult.screen.tsx
+++ src/screens/ScanResult.screen.tsx
@@ -1,5 +1,5 @@
 import CustomText from '@/components/base/CustomText';
-import {useCallback, useEffect, useState} from 'react';
+import {useCallback, useEffect, useMemo, useState} from 'react';
 import {
   View,
   StyleSheet,
@@ -20,24 +20,20 @@ import {locationStatusType} from '@/hooks/queries/useScan';
 
 type navigationProps = NavigationProp<HomeStackParamList>;
 
-type MutationStatusType = 'IN_SHELTER' | 'OUT_SHELTER' | '';
-
-interface ModalResult {
-  IN_SHELTER: string;
-  OUT_SHELTER: string;
-}
-
-const MUTATE_STATUS_ARR: MutationStatusType[] = ['IN_SHELTER', 'OUT_SHELTER'];
-const MODAL_RESULT: ModalResult = {
-  IN_SHELTER: '재실',
-  OUT_SHELTER: '외출',
-};
+const MUTATE_STATUS_ARR: locationStatusType[] = ['IN_SHELTER', 'OUT_SHELTER'];
 const ScanResult = () => {
   const navigation = useNavigation<navigationProps>();
   const {deepLinkData} = useScan();
   const {mutate} = useLocation();
   const [isModalVisible, setIsModalVisible] = useState<boolean>(false);
-
+  const locationStatus = useMemo(() => {
+    if (deepLinkData.locationStatus === 'IN_SHELTER') {
+      return '재실';
+    }
+    if (deepLinkData.locationStatus === 'OUT_SHELTER') {
+      return '외출';
+    }
+  }, [deepLinkData]);
   useEffect(() => {
     const {locationStatus} = deepLinkData;
     if (MUTATE_STATUS_ARR.includes(locationStatus)) {
@@ -67,9 +63,7 @@ const ScanResult = () => {
             <View style={styles.modalContainer}>
               <View style={styles.modalTextContainer}>
                 <CustomText style={styles.modalText}>
-                  {deepLinkData.locationStatus &&
-                    MODAL_RESULT[deepLinkData.locationStatus]}
-                  신청이 완료 되었습니다.
+                  {locationStatus} 신청이 완료 되었습니다.
                 </CustomText>
               </View>
               <View style={styles.modalButtonContainer}>
