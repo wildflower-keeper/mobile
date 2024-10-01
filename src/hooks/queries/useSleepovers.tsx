fb5660469f8b19d2f07bfa41719dd606dd293fd3
imfreeman1 <96756158+imfreeman1@users.noreply.github.com>
2024-10-01 23:17:37 +0900

    Bugfix/TSK-211 (#49)
    * [feat] SleepoverList 리스트 통신 구현 및 리스트 생성
    
    * [fix] SleepoverScheduleContainer 수정
    
     - 신청 내역에서 사용되는 카드와 함께 이용하기 위해 데이터 구조 변경
     - 데이터 형태 변경
     - invalidQuery 추가

---
+++ src/hooks/queries/useSleepovers.tsx
@@ -0,0 +1,56 @@
+// Utils
+import {getAccessToken} from '@/utils/api/auth';
+import {useQuery} from '@tanstack/react-query';
+import {useEffect, useState} from 'react';
+
+interface OvernightListResponseType {
+  startDate: string;
+  endDate: string;
+  reason: string;
+  sleepoverId: number;
+  cancelable: boolean;
+}
+
+const useSleepovers = () => {
+  const [token, setToken] = useState<string>('');
+  useEffect(() => {
+    (async () => {
+      const data = await getAccessToken();
+      if (data) {
+        setToken(data);
+      }
+    })();
+  }, []);
+  const {data} = useQuery<OvernightListResponseType[]>({
+    queryKey: ['sleepovers'],
+    enabled: !!token,
+    queryFn: async () => {
+      const response: Response = await fetch(
+        'https://api.wildflower-gardening.com/api/v1/homeless-app/sleepovers',
+        {
+          method: 'get',
+          headers: {
+            'Content-Type': 'application/json',
+            accept: '*/*',
+            'auth-token': token,
+          },
+        },
+      );
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+
+      const result = await response.json();
+
+      if (!result) {
+        throw new Error('SleepoverList is missing from the response');
+      }
+
+      return result;
+    },
+  });
+
+  return {data};
+};
+
+export default useSleepovers;
