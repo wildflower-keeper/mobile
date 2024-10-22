865f69d75aa6a5dd7fd98dd4c0caff44b54ddaab
reeny404 <happy2heyon@gmail.com>
2024-10-22 18:13:32 +0900

    style: 홈헤더 UI/UX 변경 - 이후 app push 구현되면 헤더 우측에는 알림 아이콘이 들어갈 예정

--- src/components/HomeHeader/index.tsx
+++ src/components/HomeHeader/index.tsx
@@ -3,7 +3,6 @@ import {StyleSheet, View} from 'react-native';
 import CustomText from '../base/CustomText';
 import {colors} from '@/constants';
 import {formatUpdateTime} from '@/utils/date/date';
-import useLocation from '@/hooks/queries/useLocation';
 
 interface HomeHeaderProps {
   shelterName: string;
@@ -12,41 +11,21 @@ interface HomeHeaderProps {
 }
 
 const HomeHeader = ({shelterName, homelessName, today}: HomeHeaderProps) => {
-  const {data: locationStatusQuery, mutate} = useLocation();
-  const locationStatus = useMemo(() => {
-    if (locationStatusQuery?.locationStatus === 'IN_SHELTER') {
-      return '재실';
-    }
-    if (locationStatusQuery?.locationStatus === 'OUT_SHELTER') {
-      return '외출';
-    }
-  }, [locationStatusQuery]);
   return (
     <View style={styles.container}>
       <View style={styles.headerContainer}>
         <View style={styles.headContainer}>
-          <CustomText textColor="white" size="xSmall">
+          <CustomText size="large" weight="heavy">
+            {homelessName}
+          </CustomText>
+          <CustomText size="xSmall">
             {shelterName}
-          </CustomText>
-          <CustomText textColor="white" size="small">
-            {formatUpdateTime(today)}
-          </CustomText>
-          <CustomText textColor="white" weight="heavy">
-            {`${homelessName}님, 반갑습니다.`}
           </CustomText>
         </View>
         <View style={styles.weatherContainer}>
           <View style={styles.weatherFlexContainer}>
-            <CustomText textColor="white" weight="heavy">
-              {`${locationStatus} 중`}
-            </CustomText>
           </View>
         </View>
-      </View>
-      <View style={styles.messageContainer}>
-        <CustomText isBadge size="small">
-          외출 시 약을 꼭 챙겨주세요!
-        </CustomText>
       </View>
     </View>
   );
@@ -56,30 +35,19 @@ const styles = StyleSheet.create({
   container: {
     flex: 0,
     paddingVertical: 20,
-    paddingHorizontal: 10,
-    backgroundColor: colors.BRIGHT_PRIMARY,
-    borderBottomLeftRadius: 30,
-    borderBottomRightRadius: 30,
+    paddingHorizontal: 16,
     width: '100%',
-    gap: 22,
   },
   headerContainer: {
     paddingHorizontal: 10,
     flex: 0,
     flexDirection: 'row',
     justifyContent: 'space-between',
+    gap: 8
   },
   headContainer: {
     flex: 0,
     gap: 6,
-  },
-  messageContainer: {
-    backgroundColor: colors.WHITE,
-    paddingHorizontal: 24,
-    paddingVertical: 14,
-    borderRadius: 30,
-    borderColor: colors.FONT_WEAK,
-    borderWidth: 1,
   },
   weatherContainer: {
     flex: 0,
