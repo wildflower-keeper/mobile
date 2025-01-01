45c2f1764485d2cf8d10186ae14d1cacdab8f771
dong1hyun <limd1238@naver.com>
2025-01-01 12:57:22 +0900

    타입 수정

--- src/components/SurveyButton.tsx
+++ src/components/SurveyButton.tsx
@@ -3,15 +3,16 @@ import {Pressable, StyleSheet, View} from 'react-native';
 import CustomText from './base/CustomText';
 import {PUT} from '@/utils/api/api';
 import {useQueryClient} from '@tanstack/react-query';
-import SurveyConfirmationModal from './SurveyConfirmationModal';
+import SurveyConfirmationModal, { SurveyPopupType } from './SurveyConfirmationModal';
 
 type SurveyButtonProps = {
   noticeId: number;
   isAnswered: boolean;
 };
+
 export function SurveyButton({noticeId, isAnswered: is}: SurveyButtonProps) {
   const [isAlreadyAnswered, setIsAlreadyAnswered] = useState<boolean>(is);
-  const [openConfirmType, setOpenConfirmType] = useState(0);
+  const [openConfirmType, setOpenConfirmType] = useState<SurveyPopupType>(null);
   const queryClient = useQueryClient();
 
   if (isAlreadyAnswered) {
@@ -31,18 +32,18 @@ export function SurveyButton({noticeId, isAnswered: is}: SurveyButtonProps) {
       setIsAlreadyAnswered(true);
       queryClient.invalidateQueries({queryKey: ['notices']});
     });
-    setOpenConfirmType(0);
+    setOpenConfirmType(null);
   };
 
   return (
     <View style={styles.surveyButtonContainer}>
       <SurveyConfirmationModal openConfirmType={openConfirmType} setOpenConfirmType={setOpenConfirmType} handleClickSurvey={handleClickSurvey} />
-      <Pressable style={styles.surveyButton} onPress={() => setOpenConfirmType(1)}>
+      <Pressable style={styles.surveyButton} onPress={() => setOpenConfirmType('불참')}>
         <CustomText size="large" weight="heavy">
           불참하기
         </CustomText>
       </Pressable>
-      <Pressable style={styles.surveyButton} onPress={() => setOpenConfirmType(2)}>
+      <Pressable style={styles.surveyButton} onPress={() => setOpenConfirmType('참여')}>
         <CustomText size="large" weight="heavy" isBadge>
           참여하기
         </CustomText>
--- src/components/SurveyConfirmationModal.tsx
+++ src/components/SurveyConfirmationModal.tsx
@@ -1,36 +1,40 @@
 import React, { Dispatch, SetStateAction, useState } from 'react';
 import { View, Text, Modal, StyleSheet, Pressable } from 'react-native';
 
+
+export type SurveyPopupType = '참여' | '불참' | null;
+
 interface Props {
-    openConfirmType: number
-    setOpenConfirmType: Dispatch<SetStateAction<number>>
+    openConfirmType: SurveyPopupType
+    setOpenConfirmType: Dispatch<SetStateAction<SurveyPopupType>>
     handleClickSurvey: (ok: boolean) => void
 }
 
 const SurveyConfirmationModal = ({ openConfirmType, setOpenConfirmType, handleClickSurvey }: Props) => {
-    const message = openConfirmType === 1 ? '불참' : '참여';
     return (
         <Modal
             transparent={true}
-            visible={openConfirmType > 0}
+            visible={openConfirmType !== null}
         >
             <View style={styles.modalOverlay}>
                 <View style={styles.container}>
-                    <Text style={styles.Message}>{`${message}하시겠습니까?`}</Text>
-                    <Pressable 
-                    onPress={() => handleClickSurvey(openConfirmType === 2)}
-                    style={styles.confirmButton}>
-                        <Text style={styles.confirmText}>
-                            {`${message}하기`}
-                        </Text>
-                    </Pressable>
-                    <Pressable 
-                    onPress={() => setOpenConfirmType(0)}
-                    style={styles.cancelButton}>
-                        <Text style={styles.cancelText}>
-                            취소
-                        </Text>
-                    </Pressable>
+                    <Text style={styles.Message}>{`${openConfirmType}하시겠습니까?`}</Text>
+                    <View style={styles.ButtonContainer}>
+                        <Pressable
+                            onPress={() => handleClickSurvey(openConfirmType === '참여')}
+                            style={styles.confirmButton}>
+                            <Text style={styles.confirmText}>
+                                {`${openConfirmType}하기`}
+                            </Text>
+                        </Pressable>
+                        <Pressable
+                            onPress={() => setOpenConfirmType(null)}
+                            style={styles.cancelButton}>
+                            <Text style={styles.cancelText}>
+                                취소
+                            </Text>
+                        </Pressable>
+                    </View>
                 </View>
             </View>
         </Modal>
@@ -78,6 +82,11 @@ const styles = StyleSheet.create({
     cancelText: {
         color: '#00bf40'
     },
+    ButtonContainer: {
+        alignItems: 'center',
+        gap: 20,
+        width: '100%'
+    }
 });
 
 export default SurveyConfirmationModal;
